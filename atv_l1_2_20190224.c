1e85b33416a4bc254ac66dca00eaf8cdc3fd17ee
DevLucca <44988588+DevLucca@users.noreply.github.com>
2019-02-27 20:57:58 -0300

    Update atv_l1_2_20190224.c

--- atv_l1_2_20190224.c
+++ atv_l1_2_20190224.c
@@ -2,15 +2,17 @@
 #include<math.h>
 
 int main(int argc,char *argv[]){
-	char nome1,nome2,nome3;
+	char nome1[30],nome2[30],nome3[30];
 	int idade1,idade2,idade3, somaIdade;
 	float mediaIdade;
-	printf("Favor informar 3 nomes e respectivas idades:");
-	scanf("%s %d %s %d %s %d",&nome1,&idade1,&nome2,&idade2,&nome3,&idade3);
+	printf("Favor informar 3 nomes: \n");
+	scanf("%s %s %s",&nome1,&nome2,&nome3);
+	printf("e respectivas idades: \n");
+	scanf("%d %d %d",&idade1,&idade2,&idade3);
 	somaIdade = idade1 + idade2 + idade3;
 	mediaIdade = somaIdade/3;
 	printf("Nomes: \n");
-	printf("%s \n %s +\n %s \n",nome1,nome2,nome3);
-	printf("Média das idades: %f",mediaIdade) ;
+	printf("%s  %s  %s \n",nome1,nome2,nome3);
+	printf("Media das idades: %f",mediaIdade);
 	return 0;
 }
