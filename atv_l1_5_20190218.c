1619b47a9cb9278cb85ec0a2d40a3a1d298cfa2d
DevLucca <44988588+DevLucca@users.noreply.github.com>
2019-03-03 18:42:16 -0300

    Update atv_l1_5_20190218.c

--- atv_l1_5_20190218.c
+++ atv_l1_5_20190218.c
@@ -6,22 +6,26 @@
 
 int main(int argc,char *argv[]){
 	
-	int tam = 5;
-	int num[tam],par[tam],imp[tam];
+	int tam = 3;
+	int num[tam],par[tam],imp[tam],teste;
 	printf("Bem vindo ao separador numerico! \nFavor inserir 4 numeros: ");
-	scanf("%d %d %d %d",&num[1],&num[2],&num[3],&num[4]);
+	scanf("%d %d %d %d",&num[0],&num[1],&num[2],&num[3]);
 	
-	for (int i = 1; i < tam; i++){
-		if((num[i] % 2 )== 0){
+	for (int i = 0; i < tam; i++){
+	    teste = num[i] % 2
+		if(teste== 0){
 			par[i] = num[i];
 		} else {
 			imp[i] = num[i];
 		}
 	}
 	
-	for (int i = 1; i < tam; i++){
-	printf("\nArray de pares:( %d ) ",par[i]);
+	
+	for (int i = 0; i < tam; i++){
+	printf("\nArray de pares:( %d ) ",par[i]);}
+	for (int i = 0; i < tam; i++){
 	printf("\nArray de impares:( %d ) ",imp[i]);
 	}
+	
 return 0;
 }
