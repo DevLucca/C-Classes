7b21f8a7fda753f81ab4c9bf7a3eb4a242bed153
DevLucca <44988588+DevLucca@users.noreply.github.com>
2019-03-07 21:20:32 -0300

    Add files via upload

---
+++ atv_l1_7_20190307.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+#include<time.h>
+
+// gcc teste.c -std=c99
+
+int main(int argc,char *argv[]){
+	
+	float lq,area,perim;
+	printf("Bem vindo a calculadora de area e perimetro!\nFavor inserir o tamanho do lado do seu quadrado: ");
+	scanf("%f",&lq);
+	printf("Calculando...\n");
+	//----------------------------------------------------------------------//
+	sleep(3);
+	
+	area = lq * lq * lq;
+	perim = lq * 4;
+	
+	sleep(1);
+	
+	printf("Sua area é %0.2f!\n",area);
+	printf("Seu perimetro é %0.2f!\n",perim);
+	
+	printf("Ate a proxima!");
+	
+return 0;
+}
