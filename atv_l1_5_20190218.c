c9834dc1ac36799a604cef8aa0176e569a280b18
DevLucca <44988588+DevLucca@users.noreply.github.com>
2019-03-13 19:04:45 -0300

    Update atv_l1_5_20190218.c

--- atv_l1_5_20190218.c
+++ atv_l1_5_20190218.c
@@ -6,26 +6,63 @@
 
 int main(int argc,char *argv[]){
 	
-	int tam = 3;
+	int tam = 4, a = 0, b = 0, div = 0, soma = 0,maiorpar = 0, menor = 0, maior = 0;
+	float med = 0;
 	int num[tam],par[tam],imp[tam],teste;
 	printf("Bem vindo ao separador numerico! \nFavor inserir 4 numeros: ");
 	scanf("%d %d %d %d",&num[0],&num[1],&num[2],&num[3]);
 	
 	for (int i = 0; i < tam; i++){
-	    teste = num[i] % 2
-		if(teste== 0){
-			par[i] = num[i];
+	    par[i] = 0;
+	    imp[i] = 0;
+	}
+	
+	for (int i = 0; i < tam; i++){
+	    teste = num[i] % 2;
+		if(teste == 0){
+			par[a] = num[i];
+			a++;
 		} else {
-			imp[i] = num[i];
+			imp[b] = num[i];
+			b++;
 		}
 	}
 	
 	
 	for (int i = 0; i < tam; i++){
-	printf("\nArray de pares:( %d ) ",par[i]);}
-	for (int i = 0; i < tam; i++){
-	printf("\nArray de impares:( %d ) ",imp[i]);
+	    if(par[i]==0){
+	        continue;
+	    } else {
+	printf("\nArray de pares:( %d ) ",par[i]);
+	    }
 	}
+	for (int i = 0; i < tam; i++){
+	    if(imp[i]==0){
+	        continue;
+	    } else {
+	printf("\nArray de impares:( %d ) ",imp[i]);      
+	    }
+	}
+	
+	for(int i = 0; i < tam; i++){
+	    if(imp[i]!=0){
+	        div += 1;
+	    }    
+	}
+	
+	soma = imp[0] + imp[1] + imp[2] + imp[3];
+	med = soma/div;
+	printf("\nMedia dos impares: %0.2f",med);
+
+	maiorpar = par[0];
+	for(int i = 0; i < tam; i++){
+	    
+	    if(par[i]>maiorpar){
+	        maiorpar = par[i];
+	    } 
+	}
+	
+		printf("\nMaior par: %d",maior);
 	
 return 0;
 }
