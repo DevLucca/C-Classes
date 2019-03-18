ebeee55f69ee2fe09141039e8f9e966971b8b4bf
DevLucca <44988588+DevLucca@users.noreply.github.com>
2019-03-18 00:12:40 -0300

    Create atv_triangles_20190318.c

---
+++ atv_triangles_20190318.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+
+// gcc teste.c -std=c99
+
+int main(int argc,char *argv[]){
+   
+   float a, b, c;
+   printf("BEM VINDO AO INDICADOR TRIANGULAR\n-------------------------------------------------\nFAVOR INSERIR A, B e C PARA CONTINUAR:\n");
+   printf("A: B: C:");
+   scanf("%f %f %f",&a,&b,&c);
+   if(a <= 0 || b <= 0 || c <= 0){
+       printf("VALORES NEGATIVOS, INPUT INVALIDO!");
+   } else {
+   if(a >= b + c){
+       printf("NAO É UM TRIANGULO\nRECOMECAR...");
+   } else
+   if(pow(a,2) == pow(b,2) + pow(c,2)){
+       printf("TRIANGULO RETANGULO\n");
+   } 
+   if(pow(a,2) > pow(b,2) + pow(c,2)){
+       printf("TRIANGULO OBTUSANGULO\n");
+   } 
+   if(pow(a,2) < pow(b,2) + pow(c,2)){
+       printf("TRIANGULO ACUTANGULO\n");
+   } 
+   if(a == b && b == c){
+       printf("TRIANGULO EQUILATERO\n");
+   }
+   if((a == b && c != a && c != b)||(a == c && b != a && b != c)||(c == b && a != b && a != c)){
+       printf("TRIANGULO ISOCELES\n");
+   }
+   }
+return 0;
+}
