476bedbba14032b99839a76bf6fc8c7212211c1c
DevLucca <44988588+DevLucca@users.noreply.github.com>
2019-04-14 15:29:02 -0300

    Update atv_l1_2_20190224.c

--- Listas/Lista-1/atv_l1_2_20190224.c
+++ Listas/Lista-1/atv_l1_2_20190224.c
@@ -9,8 +9,7 @@ int main(int argc,char *argv[]){
 	scanf("%s%d%s%d%s%d",nome1,&idade1,nome2,&idade2,nome3,&idade3);
 	somaIdade = idade1 + idade2 + idade3;
 	mediaIdade = somaIdade/3;
-	printf("Nomes: \n");
-	printf("[1] %s \n[2] %s \n[3] %s \n",nome1,nome2,nome3);
+	printf("Nomes: \n[1] %s \n[2] %s \n[3] %s \n",nome1,nome2,nome3);
 	printf("Media das idades: %0.2f",mediaIdade);
 	return 0;
 }
