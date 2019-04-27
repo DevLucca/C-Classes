bd523b7160c32082c06b360345f0a9c3182afd80
DevLucca <44988588+DevLucca@users.noreply.github.com>
2019-04-27 20:42:09 -0300

    Update atv_l3_10_20190426.c

--- Listas/Lista-3/atv_l3_10_20190426.c
+++ Listas/Lista-3/atv_l3_10_20190426.c
@@ -18,7 +18,7 @@ int main() {
   qt_tot = qt_tot - 1;
   printf("Quantidade de positivos = [%d de %d]\n",qt_pos,qt_tot);
   printf("Quantidade de negativos = [%d de %d]\n",qt_neg,qt_tot);
-  printf("Soma de positivos = [%lf]\n",pos);
-  printf("Soma de negativos = [%lf]\n",neg);
+  printf("Soma de positivos = [%.2lf]\n",pos);
+  printf("Soma de negativos = [%.2lf]\n",neg);
   return 0;
 }
