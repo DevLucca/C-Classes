f9c2b72ad45d262389c9e9f54cf85ec5b6a13dfe
DevLucca <44988588+DevLucca@users.noreply.github.com>
2019-04-26 21:08:27 -0300

    Create atv_l3_10_20190426.c

---
+++ Listas/Lista-3/atv_l3_10_20190426.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+  double n,neg = 0,pos = 0;
+  int qt_neg = 0, qt_pos = 0,qt_tot = 0;
+  do{
+    scanf("%lf",&n);
+    if(n < 0){
+      neg+=n;
+      qt_neg++;
+      qt_tot++;
+    } else if(n > 0){
+      pos+=n;
+      qt_pos++;
+      qt_tot++;
+    }
+  }
+  while (n);
+  printf("Quantidade de positivos = [%d de %d]\n",qt_pos,qt_tot);
+  printf("Quantidade de negativos = [%d de %d]\n",qt_pos,qt_tot);
+  printf("Soma de positivos = [%lf]\n",pos);
+  printf("Soma de negativos = [%lf]\n",neg);
+  return 0;
+}
